52f214f3ae3f959243bdb905d2249b3bae30d910
AmazinAxel <axel@amazinaxel.com>
2025-10-20 19:34:45 -0700

    feat: new dtb method of application

--- main.nix
+++ main.nix
@@ -6,34 +6,38 @@
   networking.hostName = "alechandheld";
   #home-manager.users.alec.imports = [ ./hm.nix ];
 
-  #nixpkgs.config.allowUnfree = true;
+  nixpkgs.config.allowUnfree = true;
   boot = {
     loader = {
       grub.enable = lib.mkForce false;
       generic-extlinux-compatible.enable = lib.mkForce true;
     };
-    #kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
   };
   sdImage.compressImage = false;
 
   hardware.deviceTree = {
     enable = false;
     name = "allwinner/h700-anbernic-rg35xxh.dtb";
-
-    package =
-      pkgs.runCommandCC "h700-dts" {
-        inherit (config.boot.kernelPackages.kernel) src;
-        nativeBuildInputs = [ pkgs.dtc ];
-      } ''
-        unpackPhase
-        cd "$sourceRoot"
-        DTS=arch/arm64/boot/dts/h700-anbernic-rg35xxh.dts
-        cp "${./sun50i-h700-anbernic-rg35xx-h.dtb}" "$DTS"
-        mkdir -p "$out/rockchip"
-        $CC -E -nostdinc -Iinclude -undef -D__DTS__ -x assembler-with-cpp "$DTS" | \
-          dtc -I dts -O dtb -@ -o "$out/allwinner/h700-anbernic-rg35xxh.dtb"
-      '';
+    dtbSource = ./sun50i-h700-anbernic-rg35xx-h.dtb;
   };
+
+  #boot.kernelPatches =
+  #  [
+  #    {
+  #      name = "h700-config";
+  #      patch = null;
+  #      extraStructuredConfig = {
+  #        DRM = lib.kernel.yes;
+  #        INPUT_JOYSTICK = lib.kernel.yes;
+  #        JOYSTICK_SINGLEADCJOY = lib.kernel.yes;
+  #        EMBEDDED = lib.kernel.yes;
+  #      };
+  #    }
+  #  ];# ++ builtins.map (p: {
+    #  name = builtins.elemAt (pkgs.lib.splitString "." (builtins.baseNameOf p.url)) 0;
+    #  patch = pkgs.fetchpatch p;
+    #}) (import ./rocknix-patches.nix);
 
   environment.systemPackages = with pkgs; [
     gitMinimal
---
+++ rocknix-patches.nix
@@ -0,0 +1,10 @@
+[
+{
+  sha256 = "sha256-QJIbr5Loln+eeeeeeeee=";
+  url = "";
+}
+{
+  sha256 = "";
+  url = "https://raw.githubusercontent.com/";
+}
+]
